823870a5ee53f924f3c9bced752055fdb8f0751d
Benjamin CORNOU <Bencordu29@gmail.com>
2023-08-22 20:47:31 +0200

    Update ipt.sh

--- ipt.sh
+++ ipt.sh
@@ -16,8 +16,9 @@ CMD_COMMENT="-m comment --comment=srcds-hardening"
 CMD_PORTS="-m multiport --dports 27000:29000"
 
 
+ipset create blacklist    hash:ip      timeout 0 -! || true
 ipset create permatrusted hash:ip      timeout 0 -! || true
-ipset create  signed_on   hash:ip,port timeout 5 -! || true
+ipset create signed_on    hash:ip,port timeout 5 -! || true
 
 
 # Log and DROP invalid packets
@@ -25,13 +26,20 @@ iptables -I PREROUTING 1 -t mangle -p all $CMD_COMMENT \
 	-m state --state INVALID \
 	-j LOG \
 	-m limit --limit 2/min --log-ip-options --log-prefix "<|srcds-ipt|> INVALID PKT: "
-	
+
 iptables -I PREROUTING 2 -t mangle -p all $CMD_COMMENT \
 	-m state --state INVALID \
 	-j DROP
 
+
+# Blacklist
+iptables -I PREROUTING 1 $CMD_BASE $CMD_COMMENT \
+  -m set --match-set blacklist src \
+  -j DROP
+
+
 # data: TSEQ | hashlimit: srcip,dstport | limit: 6/min | j: DROP
-iptables -I PREROUTING 1 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
+iptables -I PREROUTING 2 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-m string --algo bm --hex-string "|$PKT_A2S|" \
 	-m hashlimit \
 		--hashlimit-name tseq \
@@ -41,53 +49,53 @@ iptables -I PREROUTING 1 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-j DROP
 
 # data: TSEQ | limit: 20/sec | j: ACCEPT | else: DROP
-iptables -I PREROUTING 2 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
+iptables -I PREROUTING 3 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-m string --algo bm --hex-string "|$PKT_A2S|" \
 	-m limit --limit 20/sec --limit-burst 50 \
 	-j ACCEPT
 
-iptables -I PREROUTING 3 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
+iptables -I PREROUTING 4 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-m string --algo bm --hex-string "|$PKT_A2S|" \
 	-j DROP
-	
+
 
 # Log flair signon and add to signed_on set
-iptables -I PREROUTING 4 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
+iptables -I PREROUTING 5 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-m string --algo bm --hex-string "|$PKT_LOG|" \
 	-m length --length 48 --from 26 --to 48 \
 	-j LOG \
 	-m limit --limit 250/min --log-ip-options --log-level error --log-prefix "<|srcds-ipt|> signon: "
 
-iptables -I PREROUTING 5 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
+iptables -I PREROUTING 6 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-m string --algo bm --hex-string "|$PKT_LOG|" \
 	-m length --length 48 --from 26 --to 48 \
 	-j SET --add-set signed_on src,dst --timeout 5
 
 
 # Log and DROP small packets
-iptables -I PREROUTING 6 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
+iptables -I PREROUTING 7 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-m length --length 0:32 \
 	-j LOG \
 	-m limit --limit 2/min --log-ip-options --log-prefix "<|srcds-ipt|> len < 32: "
 
-iptables -I PREROUTING 7 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
+iptables -I PREROUTING 8 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-m length --length 0:32 \
 	-j DROP
 
 
 # Log and DROP bigass packets
-iptables -I PREROUTING 8 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
+iptables -I PREROUTING 9 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-m length --length 2521:65535 \
 	-j LOG \
 	-m limit --limit 2/min --log-ip-options --log-prefix "<|srcds-ipt|> len > 2521: "
 
-iptables -I PREROUTING 9 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
+iptables -I PREROUTING 10 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-m length --length 2521:65535 \
 	-j DROP
 
 
 # Rate limit permatrusted (ACCEPT should be removed)
-iptables -I PREROUTING 10 $CMD_BASE $CMD_COMMENT \
+iptables -I PREROUTING 11 $CMD_BASE $CMD_COMMENT \
 	-m set --match-set permatrusted src \
 	-m hashlimit \
 		--hashlimit-name validated_speedlimit \
@@ -96,13 +104,13 @@ iptables -I PREROUTING 10 $CMD_BASE $CMD_COMMENT \
 		--hashlimit-burst 80 \
 	-j DROP
 
-iptables -I PREROUTING 11 $CMD_BASE $CMD_COMMENT \
+iptables -I PREROUTING 12 $CMD_BASE $CMD_COMMENT \
 	-m set --match-set permatrusted src \
 	-j ACCEPT
 
 
 # Rate limit flair loged (should be replaced with a very strict burst)
-iptables -I PREROUTING 12 $CMD_BASE $CMD_COMMENT \
+iptables -I PREROUTING 13 $CMD_BASE $CMD_COMMENT \
 	-m set --match-set signed_on src,dst \
 	-m hashlimit \
 		--hashlimit-name signedon_speedlimit \
@@ -111,13 +119,13 @@ iptables -I PREROUTING 12 $CMD_BASE $CMD_COMMENT \
 		--hashlimit-burst 20 \
 	-j DROP
 
-iptables -I PREROUTING 13 $CMD_BASE $CMD_COMMENT \
+iptables -I PREROUTING 14 $CMD_BASE $CMD_COMMENT \
 	-m set --match-set signed_on src,dst \
 	-j ACCEPT
 
 
 # Not loged udp spam limit
-iptables -I PREROUTING 14 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
+iptables -I PREROUTING 15 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-m hashlimit \
 		--hashlimit-name speedlimit \
 		--hashlimit-mode srcip,dstport \
@@ -127,8 +135,8 @@ iptables -I PREROUTING 14 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-m set ! --match-set signed_on src,dst \
 	-j LOG \
 	-m limit --limit 2/min --log-ip-options --log-prefix "<|srcds-ipt|> udp spam: "
- 
-iptables -I PREROUTING 15 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
+
+iptables -I PREROUTING 16 $CMD_BASE $CMD_COMMENT $CMD_PORTS \
 	-m hashlimit \
 		--hashlimit-name speedlimit \
 		--hashlimit-mode srcip,dstport \
